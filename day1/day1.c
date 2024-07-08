8f0ebbbc67851fe0d186730471edb0e8d5e50220
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-07-07 22:06:22 -0300

    first implementation of ordered list

--- day1/day1.c
+++ day1/day1.c
@@ -4,39 +4,95 @@
 
 const int MAX_LEN = 100;
 
+struct Elve {
+  int calories;
+  struct Elve *next;
+};
+
+struct Elve *new_item(int calories) {
+  struct Elve *new_elve;
+
+  new_elve = (struct Elve *) malloc(sizeof(struct Elve));
+
+  new_elve->calories = calories;
+  new_elve->next = NULL;
+
+  return new_elve;
+}
+
+// struct Elve *add_front(struct Elve *listp, struct Elve *new_elve) {
+//   new_elve->next = listp;
+//   return new_elve;
+// }
+
+struct Elve *add_front(struct Elve *listp, struct Elve *new_elve) {
+  if (listp == NULL) {
+    new_elve->next = listp;
+    return new_elve;
+  } else {
+    if (new_elve->calories < listp->calories) {
+
+      new_elve->next = listp;
+      return new_elve;
+    } else {
+      struct Elve *head = listp;
+
+      for (; head->next != NULL; head = head->next) {
+        if (head->calories < new_elve->calories && head->next->calories > new_elve->calories) {
+          new_elve->next = head->next;
+          head->next = new_elve;
+          break;;
+        }
+      }
+
+      if (head->next == NULL) {
+        head->next = new_elve;
+      }
+      return listp;
+    }
+  }
+}
+
 int main() {
+  struct Elve *elves = NULL;
+
   FILE *file;
   char line[MAX_LEN], *result;
 
-  int current_index = 0;
+  struct CalorieRanking {
+    int first;
+    int second;
+    int third;
 
-  struct Foo {
-    int index;
-    int counter;
     int tracker;
   };
 
-  struct Foo foo = { 0, 0, 0 };
+  struct CalorieRanking foo = { 0, 0, 0, 0 };
 
-  file = fopen("./food.txt", "r");
+  file = fopen("./test.txt", "r");
 
   while ((result = fgets(line, MAX_LEN, file)) != NULL) {
     if (strcmp(result, "\n") == 0) {
-      if (foo.tracker == foo.counter) {
-        foo.index = current_index;
-      }
+      elves = add_front(elves, new_item(foo.tracker));
 
-      current_index++;
       foo.tracker = 0;
     } else {
       foo.tracker += atoi(result);
-      if (foo.tracker > foo.counter) {
-        foo.counter = foo.tracker;
-      }
     }
   }
 
-  printf("FOO IS: counter[%i] | index[%i]\n", foo.counter, foo.index);
+  elves = add_front(elves, new_item(foo.tracker));
+  // printf("VALUE before loop IS: %d\n", elves->calories);
+  while (elves != NULL) {
+    printf("VALUE IS: %d\n", elves->calories);
+    elves = elves->next;
+  }
+
+  // printf("FIRST: counter[%i]\n", foo.first);
+  // printf("SECOND: counter[%i]\n", foo.second);
+  // printf("THIRD: counter[%i]\n", foo.third);
+
+  // printf("FOO IS: counter[%i]\n", foo.first + foo.second + foo.third);
 
   if (fclose(file)) {
     perror("Error on close file");
