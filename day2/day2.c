e3dbe71f6ceb924dd37a1fb04186d47b7f03e9ce
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-07-20 10:42:11 -0300

    first resolution of day 2

--- day2/day2.c
+++ day2/day2.c
@@ -1,36 +1,79 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-enum Values {
+enum game_values {
+  LOST = 0,
+  DRAW = 3,
+  WIN = 6
+};
+
+enum game {
   ROCK = 1,
   PAPER = 2,
   SCISSORS = 3
 };
 
+char opponent_moves[] = { 'A', 'B', 'C' };
+char moves[] = { 'X', 'Y', 'Z' };
+int values[] = { ROCK, PAPER, SCISSORS };
+
 const int SIZE = 4;
 const char *separator = " ";
 
 int main() {
+  int accumulator = 0;
+  int opponent_count = 0;
   int count = 0;
-  FILE *stream;
-  char line[SIZE], *token;
 
-  stream = fopen("./test.txt", "r");
+  FILE *stream;
+  char line[SIZE], *token, *token_test;
+
+  stream = fopen("./strategy.txt", "r");
 
   while ((fgets(line, SIZE, stream)) != NULL) {
     if (strcmp(line, "\n") == 0) {
-      printf("calculate the value...\n");
-      printf("should update the value of count\n");
+      accumulator += count;
 
+      opponent_count = 0;
+      count = 0;
       continue;
     };
 
     token = strtok(line, separator);
+    token_test = strtok(NULL, separator);
 
-    printf("LINE IS: %s\n", line);
-    while (token != NULL) {
-      printf("TOKEN IS: %s\n", token);
-      token = strtok(NULL, separator);
-    }
+    int opponent = (int) token[0];
+    int current_game = (int) token_test[0];
+
+     for (int i = 0; i < 3; i++) {
+       if (opponent == opponent_moves[i]) {
+         opponent_count += values[i];
+       }
+     }
+
+     for (int i = 0; i < 3; i++) {
+       if (current_game == moves[i]) {
+         count += values[i];
+       }
+     }
+
+     if (opponent_count == ROCK && count == PAPER) {
+        count += WIN;
+     } else if (opponent_count == ROCK && count == SCISSORS) {
+        opponent_count += WIN;
+     } else if (opponent_count == PAPER && count == ROCK) {
+        opponent_count += WIN;
+     } else if (opponent_count == PAPER && count == SCISSORS) {
+        count += WIN;
+     } else if (opponent_count == SCISSORS && count == ROCK) {
+        count += WIN;
+     } else if (opponent_count == SCISSORS && count == PAPER) {
+        opponent_count += WIN;
+     } else {
+        count += DRAW;
+     }
   }
+
+  printf("The result is: %d\n", accumulator);
 }
