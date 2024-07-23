1ad76e090649e4d13d32d1472dba571a97324931
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-07-23 14:18:27 -0300

    refac: remove old code and better name to data structures

--- day2/day2_part2.c
+++ day2/day2_part2.c
@@ -2,60 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 
-enum game_values {
-  LOST = 0,
+enum result_values {
   DRAW = 3,
   WIN = 6
 };
 
-enum game {
+enum play_values {
   ROCK = 1,
   PAPER = 2,
   SCISSORS = 3
 };
 
-struct Game {
+struct results {
   const char WIN;
   const char LOSE;
   const char DRAW;
 };
 
-struct Moves {
+struct moves {
   const char ROCK;
   const char PAPER;
   const char SCISSORS;
 };
 
-const int MOVE_COUNT = 3;
-int const VALUES[] = { ROCK, PAPER, SCISSORS };
-
-char opponent_moves[] = { 'A', 'B', 'C' };
-
 const int SIZE = 4;
 const char *separator = " ";
 
-_Bool player_wins(int player_move, int opponent_move) {
-  int result = opponent_move - player_move;
-  return result == 2 || result == -1;
-}
-
-_Bool is_draw(int player_move, int opponent_move) {
-  return (player_move - opponent_move) == 0;
-}
-
-int play_value(int play, char *moves) {
-  for (int i = 0; i < MOVE_COUNT; i++) {
-    if (play == moves[i]) return VALUES[i];
-  }
+int play_value(int play, struct moves moves) {
+  if (moves.ROCK == play) return ROCK;
+  if (moves.PAPER == play) return PAPER;
+  if (moves.SCISSORS == play) return SCISSORS;
 
   return 0;
 }
 
 int main() {
-  struct Game game = { 'Z', 'X', 'Y' };
-  struct Moves moves = { 'A', 'B', 'C' };
+  struct results results = { 'Z', 'X', 'Y' };
+  struct moves moves = { 'A', 'B', 'C' };
 
-  int accumulator = 0, current_game = 0;
+  int accumulator = 0, current_play_value = 0;
 
   FILE *stream;
   char line[SIZE], *p_opponent_play, *p_play;
@@ -64,9 +49,9 @@ int main() {
 
   while ((fgets(line, SIZE, stream)) != NULL) {
     if (strcmp(line, "\n") == 0) {
-      accumulator += current_game;
+      accumulator += current_play_value;
 
-      current_game = 0;
+      current_play_value = 0;
       continue;
     };
 
@@ -76,38 +61,16 @@ int main() {
     int opponent_play = (int) p_opponent_play[0];
     int play = (int) p_play[0];
 
-    if (play == game.DRAW) {
-      current_game = play_value(opponent_play, opponent_moves);
-      current_game += DRAW;
+    if (play == results.DRAW) {
+      current_play_value = play_value(opponent_play, moves) + DRAW;
       continue;
     }
 
-    if (opponent_play == moves.ROCK) {
-      if (play == game.LOSE) {
-        current_game += SCISSORS;
-      } else {
-        current_game += PAPER;
-        current_game += WIN;
-      }
-    }
+    _Bool is_victory = play == results.WIN;
 
-    if (opponent_play == moves.PAPER) {
-      if (play == game.LOSE) {
-        current_game += ROCK;
-      } else {
-        current_game += SCISSORS;
-        current_game += WIN;
-      }
-    }
-
-    if (opponent_play == moves.SCISSORS) {
-      if (play == game.LOSE) {
-        current_game += PAPER;
-      } else {
-        current_game += ROCK;
-        current_game += WIN;
-      }
-    }
+    if (opponent_play == moves.ROCK) current_play_value = is_victory ? PAPER + WIN : SCISSORS;
+    if (opponent_play == moves.PAPER) current_play_value = is_victory ? SCISSORS + WIN : ROCK;
+    if (opponent_play == moves.SCISSORS) current_play_value = is_victory ? ROCK + WIN : PAPER;
   }
 
   fclose(stream);
