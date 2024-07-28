cc59149507293692a1aab331b45cbeaa237e942f
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-07-28 10:43:12 -0300

    feat: first resolution

--- day3/day3.c
+++ day3/day3.c
@@ -1,38 +1,83 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
 
+struct Node {
+  int value;
+  struct Node *next;
+};
+
+struct Node *new_node(int value) {
+  struct Node *new_node;
+
+  new_node = malloc(sizeof(struct Node));
+
+  new_node->value = value;
+  new_node->next = NULL;
+
+  return new_node;
+}
+
+struct Node *insert(struct Node *listp, struct Node *new_node) {
+  new_node->next = listp;
+  return new_node;
+}
+
+int has_value(struct Node *listp, int value) {
+  if (!listp) return 0;
+
+  struct Node *head = listp;
+
+  if (head->next == NULL) {
+    if (value == head->value) return head->value;
+  }
+
+  for (; head->next != NULL; head = head->next) {
+    if (value == head->value) return head->value;
+  }
+
+  return 0;
+}
+
 int main() {
+  struct Node *values = NULL;
+
   FILE *stream;
   char *line = NULL;
+  int sum = 0;
+
+  int count = 0;
   
   size_t len = 0;
   ssize_t read;
 
-
-  stream = fopen("./test.txt", "r");
+  stream = fopen("./puzzle.txt", "r");
 
   while ((read = getline(&line, &len, stream)) != -1) {
     int string_size = read - 1;
-    int mid = string_size / 2;
+    int mid = (string_size / 2) - 1;
 
-    int start = 0, end = 0;
+    for (int i = 0; i <= mid; i++) {
+      for (int j = mid + 1; j <= string_size; j++) {
+        if (line[i] == line[j] && !has_value(values, line[i])) {
+          int value = 0;
 
-    for (int i = 0; i < string_size; i++) {
-      if (i >= mid) end++;
-      else start++;
+          if (isupper(line[i])) value = line[i] - 38;
+          else value = line[i] - 96;
+
+          values = insert(values, new_node(line[i]));
+          sum += value;
+        }
+      }
     }
 
-    printf("SIZE: %i\n", string_size);
-    printf("FIRST PART: %i\n", start);
-    printf("LAST PART: %i\n", end);
-    printf("THIS IS A NEW LINE [%s]\n", line);
-
-    printf("The first caracter is %c\n", line[0]);
-    printf("The last caracter is %c\n", line[read - 2]);
-    printf("--------------------------------------\n");
+    count++;
+    values = NULL;
   }
+
+  printf("RESULT: %i\n", sum);
 
   free(line);
   fclose(stream);
