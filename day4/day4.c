a0741dd22a27de1a39e147f1ecfa9c3023a835ff
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-08-17 10:46:24 -0300

    feat(day4): split sections

---
+++ day4/day4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Sections {
+  char* first;
+  char* second;
+};
+
+struct Sections get_sections(char* line) {
+  static const char* DELIM = ",";
+  struct Sections result;
+
+  result.first = strtok(line, DELIM);
+  result.second = strtok(NULL, DELIM);
+
+  return result;
+}
+
+int main() {
+  FILE *stream;
+  char *line = NULL;
+
+  size_t len = 0;
+  ssize_t read;
+
+  stream = fopen("./test.txt", "r");
+
+  while ((read = getline(&line, &len, stream)) != -1) {
+    struct Sections sections = get_sections(line);
+
+
+    printf("VALUE OF FIRST: %s | SECOND: %s", sections.first, sections.second);
+
+  }
+
+  free(line);
+  fclose(stream);
+}
