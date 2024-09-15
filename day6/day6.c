9fa4142b75c14273d5c5266965876fb3122ced70
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-09-15 13:08:17 -0300

    feat: get_chunk helper

--- day6/day6.c
+++ day6/day6.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char* get_chunk(char* result, char* line, int start, int chunk_size) {
+  for (int i = 0; i < chunk_size; i++) {
+    result[i] = line[start + i]; 
+  }
+
+  return result;
+}
+
 int day6(char *path) {
   FILE *stream;
   char *line = NULL;
@@ -13,8 +21,10 @@ int day6(char *path) {
   if ((stream = fopen(path, "r")) == NULL) return -1;
 
   while ((read = getline(&line, &len, stream)) != -1) {
-    for (int i = 0; i < read - 1; i++) {
-      printf("I is %c\n", line[i]);
+    for (int i= 0; i < read - 1; i++) {
+      // const char* result = get_chunk(line, read, i);
+
+      // printf("RESULT %s\n", result);
     }
   }
 
--- day6/day6.h
+++ day6/day6.h
@@ -2,5 +2,6 @@
 #define MODULE_day6
 
 int day6(char *path);
+char* get_chunk(char* result, char* line, int start, int chunk_size);
 
 #endif
