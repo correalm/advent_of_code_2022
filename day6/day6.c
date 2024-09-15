e2f7284388c6d3bcccb2d082f6e490e13a02727c
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-09-15 08:57:50 -0300

    feat: get the correct length of line

--- day6/day6.c
+++ day6/day6.c
@@ -8,10 +8,14 @@ int day6(char *path) {
   char *line = NULL;
 
   size_t len = 0;
+  ssize_t read;
 
   if ((stream = fopen(path, "r")) == NULL) return -1;
 
-  while (getline(&line, &len, stream) != -1) {
+  while ((read = getline(&line, &len, stream)) != -1) {
+    for (int i = 0; i < read - 1; i++) {
+      printf("I is %c\n", line[i]);
+    }
   }
 
   free(line);
