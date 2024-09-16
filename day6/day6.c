f301597119c63ae5c7f7998379e47c1fedcb1322
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-09-16 07:49:17 -0300

    feat: validate_chunk helper

--- day6/day6.c
+++ day6/day6.c
@@ -1,14 +1,27 @@
+#include "day6.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const char* get_chunk(char* result, char* line, int start, int chunk_size) {
+enum { CHUNK_SIZE = 4 };
+
+char* get_chunk(char* result, char* line, int start, int chunk_size) {
   for (int i = 0; i < chunk_size; i++) {
     result[i] = line[start + i]; 
   }
 
   return result;
+}
+
+int validate_chunk(char *chunk, int chunk_size) {
+  for (int i = 0; i < chunk_size; i++) {
+    for (int j = chunk_size - 1; j > i; j--) {
+      if (chunk[i] == chunk[j]) return 0;
+    }
+  }
+
+  return 1;
 }
 
 int day6(char *path) {
@@ -18,16 +31,19 @@ int day6(char *path) {
   size_t len = 0;
   ssize_t read;
 
+  char *chunk = malloc(4 * sizeof(typeof(char)));
+
   if ((stream = fopen(path, "r")) == NULL) return -1;
 
   while ((read = getline(&line, &len, stream)) != -1) {
-    for (int i= 0; i < read - 1; i++) {
-      // const char* result = get_chunk(line, read, i);
+    for (int i = 0; i < read - 1; i++) {
+      get_chunk(chunk, line, i, CHUNK_SIZE);
 
-      // printf("RESULT %s\n", result);
+      if (validate_chunk(chunk, CHUNK_SIZE) > 0) return CHUNK_SIZE + i;
     }
   }
 
+  free(chunk);
   free(line);
   fclose(stream);
 
--- day6/day6.h
+++ day6/day6.h
@@ -3,5 +3,6 @@
 
 int day6(char *path);
 char* get_chunk(char* result, char* line, int start, int chunk_size);
+int validate_chunk(char* chunk, int chunk_size);
 
 #endif
