9d17adb72a52be7b91314f2242ac6b8dc10dd767
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-09-25 08:19:53 -0300

    feat: define module interface and first functions to deal with dirs

--- day7/day7.c
+++ day7/day7.c
@@ -1,4 +1,27 @@
+#include <stdlib.h>
 #include "day7.h"
+
+struct Dir *create_dir( char *name, struct Dir *parent ) {
+  struct Dir *new_dir;
+
+  new_dir = malloc(sizeof(struct Dir));
+
+  new_dir->name = name;
+  new_dir->parent = parent;
+
+  return new_dir;
+};
+
+void insert_file( struct Dir *dir, struct Node *node ) {
+  node->parent = dir;
+
+  if (dir->children) {
+    node->next = dir->children;
+    dir->children = node;
+  }
+
+  dir->children = node;
+};
 
 int day7( char *path ){
   return 95437;
--- day7/day7.h
+++ day7/day7.h
@@ -1,6 +1,23 @@
-#ifndef MODULE_day7
-#define MODULE_day7
+#ifndef _DAY7_H_
+#define _DAY7_H_
 
-int day7(char *path);
+struct Node {
+  long size;
+  char *name;
+  struct Node *next;
+  struct Dir *parent;
+};
 
-#endif
+struct Dir {
+  char *name;
+  struct Node *children;
+  struct Dir *parent;
+  struct Dir *next;
+};
+
+
+struct Dir *create_dir ( char* name, struct Dir* parent );
+void insert_file ( struct Dir* dir, struct Node* file );
+int day7 (char *path);
+
+#endif /* _DAY7_H_ */
