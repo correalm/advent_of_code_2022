494f6cacc6a41c34877b910fd11926dcb6c3b45b
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-06 09:03:44 -0300

    validate commands and create new directories (something is wrong...)

--- day7/day7.go
+++ day7/day7.go
@@ -17,9 +17,15 @@ type File struct {
 type Dir struct {
   name string
   parent *Dir
-  childrens []*Dir
+  childrens []Dir
   files []File
 }
+
+const (
+  COMMAND_CD = "cd"
+  COMMAND_BACK = ".."
+  COMMAND_LS = "ls"
+)
 
 func newFile(name string, size int) File {
   var file File
@@ -28,6 +34,15 @@ func newFile(name string, size int) File {
   file.size = size
 
   return file
+}
+
+func newDir(name string, parent Dir) Dir {
+  var dir Dir
+
+  dir.name = name
+  dir.parent = &parent
+
+  return dir
 }
 
 func day7(path string) int {
@@ -49,7 +64,6 @@ func day7(path string) int {
   current_dir := root
 
   for scanner.Scan() {
-
     line := scanner.Text()
     fmt.Println(line)
 
@@ -62,7 +76,24 @@ func day7(path string) int {
 
       current_dir.files = append(current_dir.files, newFile(tokens[1], size))
 
-      fmt.Println(current_dir.files, "SIZE -> ", size, "Name -> ", tokens[1])
+      // fmt.Println(current_dir.files, "SIZE -> ", size, "Name -> ", tokens[1])
+    }
+
+    is_command, _ := regexp.MatchString(`\$`, line)
+
+    if is_command {
+      tokens := strings.Split(line, " ")
+
+      change_dir := (strings.Compare(tokens[1], COMMAND_CD) == 0) && (strings.Compare(tokens[2], COMMAND_BACK) != 0)
+
+      if change_dir {
+        dir := newDir(tokens[2], current_dir)
+        current_dir = dir
+      } else {
+        if (strings.Compare(tokens[1], COMMAND_LS) != 0) {
+          current_dir = *current_dir.parent
+        }
+      }
     }
 
 
