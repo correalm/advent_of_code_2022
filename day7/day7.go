40e228523fb52539a14df4dbec8ff45481f82a9b
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-08 10:28:27 -0300

    formatting

--- day7/day7.go
+++ day7/day7.go
@@ -10,135 +10,135 @@ import (
 )
 
 type Dir struct {
-  name string
-  parent *Dir
-  size int
-  childrens []*Dir
+	name      string
+	parent    *Dir
+	size      int
+	childrens []*Dir
 }
 
 type ExplorerCallback func(dir *Dir)
 
 const (
-  COMMAND_CD = "cd"
-  COMMAND_BACK = ".."
-  COMMAND_LS = "ls"
-  MAX_DIR_SIZE = 100000
+	COMMAND_CD   = "cd"
+	COMMAND_BACK = ".."
+	COMMAND_LS   = "ls"
+	MAX_DIR_SIZE = 100000
 )
 
 func newDir(name string, parent *Dir) *Dir {
-  var dir Dir
+	var dir Dir
 
-  dir.name = name
-  dir.size = 0
-  dir.parent = parent
+	dir.name = name
+	dir.size = 0
+	dir.parent = parent
 
-  return &dir
+	return &dir
 }
 
 func getLineData(line string) (string, string) {
-  tokens := strings.Split(line, " ")
+	tokens := strings.Split(line, " ")
 
-  if len(tokens) < 3 {
-    if strings.Compare(tokens[0], "$") == 0 {
-      return tokens[1], ""
-    }
+	if len(tokens) < 3 {
+		if strings.Compare(tokens[0], "$") == 0 {
+			return tokens[1], ""
+		}
 
-    return tokens[0], tokens[1]
-  }
+		return tokens[0], tokens[1]
+	}
 
-  return tokens[1], tokens[2]
+	return tokens[1], tokens[2]
 }
 
-func isFile(value string) (bool) {
-  match, err := regexp.MatchString(`[0-9]`, value)
+func isFile(value string) bool {
+	match, err := regexp.MatchString(`[0-9]`, value)
 
-  if err != nil {
-    return false
-  }
+	if err != nil {
+		return false
+	}
 
-  return match
+	return match
 }
 
-func isCmdChangeDir(value string) (bool) {
-  return strings.Compare(value, COMMAND_CD) == 0
+func isCmdChangeDir(value string) bool {
+	return strings.Compare(value, COMMAND_CD) == 0
 }
 
 func updateParentSize(dir *Dir) {
-  if dir.parent == nil {
-    return
-  }
+	if dir.parent == nil {
+		return
+	}
 
-  dir.parent.size += dir.size
+	dir.parent.size += dir.size
 }
 
 func explorer(dir *Dir, callback ExplorerCallback) {
-  if len(dir.childrens) == 0 {
-    callback(dir)
-  }
+	if len(dir.childrens) == 0 {
+		callback(dir)
+	}
 
-  for i := range dir.childrens {
-    current_dir := dir.childrens[i]
+	for i := range dir.childrens {
+		current_dir := dir.childrens[i]
 
-    if len(current_dir.childrens) > 0 {
-      explorer(current_dir, callback)
-    }
+		if len(current_dir.childrens) > 0 {
+			explorer(current_dir, callback)
+		}
 
-    callback(current_dir)
-  }
+		callback(current_dir)
+	}
 }
 
 func day7(path string) int {
-  file, err := os.Open(path)
+	file, err := os.Open(path)
 
-  defer file.Close()
+	defer file.Close()
 
-  if err != nil {
-    return 0
-  }
+	if err != nil {
+		return 0
+	}
 
-  scanner := bufio.NewScanner(file)
-  scanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-  var root Dir
-  root.name = "root"
-  root.parent = nil
+	var root Dir
+	root.name = "root"
+	root.parent = nil
 
-  current_dir := &root
+	current_dir := &root
 
-  for scanner.Scan() {
-    name, value := getLineData(scanner.Text())
+	for scanner.Scan() {
+		name, value := getLineData(scanner.Text())
 
-    if isFile(name) {
-      file_size, _ := strconv.Atoi(name)
+		if isFile(name) {
+			file_size, _ := strconv.Atoi(name)
 
-      current_dir.size += file_size
-    }
+			current_dir.size += file_size
+		}
 
-    if isCmdChangeDir(name) {
-      if strings.Compare(value, COMMAND_BACK) == 0 {
-        current_dir = current_dir.parent
-      } else {
-        dir := newDir(value, current_dir)
+		if isCmdChangeDir(name) {
+			if strings.Compare(value, COMMAND_BACK) == 0 {
+				current_dir = current_dir.parent
+			} else {
+				dir := newDir(value, current_dir)
 
-        current_dir.childrens = append(current_dir.childrens, dir)
-        current_dir = dir
-      }
-    }
+				current_dir.childrens = append(current_dir.childrens, dir)
+				current_dir = dir
+			}
+		}
 
-    updateParentSize(current_dir)
-  }
+		updateParentSize(current_dir)
+	}
 
-  var result int = 0
-  
-  explorer(
-    &root,
-    func (dir *Dir) {
-      if dir.size < MAX_DIR_SIZE {
-        fmt.Println("-> ", dir.name)
-        result += int(dir.size)
-      }
-    },
-  )
+	var result int = 0
 
-  return result
+	explorer(
+		&root,
+		func(dir *Dir) {
+			if dir.size < MAX_DIR_SIZE {
+				fmt.Println("-> ", dir.name)
+				result += int(dir.size)
+			}
+		},
+	)
+
+	return result
 }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -5,30 +5,30 @@ import (
 )
 
 func TestDay7PartOne(t *testing.T) {
-  tests := []struct {
-    name string
-    path string
-    expected int
-  }{
-    {
-      name: "returns the correct value with test fixture",
-      path: "./fixtures/test.txt",
-      expected: 95437,
-    },
-    {
-      name: "returns the correct value with puzzle fixture",
-      path: "./fixtures/puzzle.txt",
-      expected: 1770595,
-    },
-  }
+	tests := []struct {
+		name     string
+		path     string
+		expected int
+	}{
+		{
+			name:     "returns the correct value with test fixture",
+			path:     "./fixtures/test.txt",
+			expected: 95437,
+		},
+		{
+			name:     "returns the correct value with puzzle fixture",
+			path:     "./fixtures/puzzle.txt",
+			expected: 1770595,
+		},
+	}
 
-  for _, test := range tests {
-    t.Run(test.name, func(t *testing.T) {
-      actual := day7(test.path)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := day7(test.path)
 
-      if test.expected != actual {
-        t.Errorf("Expected: %d; Actual: %d\n", test.expected, actual)
-      }
-    })
-  }
+			if test.expected != actual {
+				t.Errorf("Expected: %d; Actual: %d\n", test.expected, actual)
+			}
+		})
+	}
 }
