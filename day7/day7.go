91774835146af73fda243b918d7e67a8c1552ffa
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-11 21:27:21 -0300

    first implementation of day7/2 really need a refac

--- day7/day7.go
+++ day7/day7.go
@@ -2,7 +2,6 @@ package day7
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -23,6 +22,8 @@ const (
 	COMMAND_BACK = ".."
 	COMMAND_LS   = "ls"
 	MAX_DIR_SIZE = 100000
+  FILE_SYSTEM_SIZE = 70000000
+  FREE_SPACE_REQUIRED = 30000000
 )
 
 func newDir(name string, parent *Dir) *Dir {
@@ -64,7 +65,7 @@ func isCmdChangeDir(value string) bool {
 }
 
 func updateParentSize(dir *Dir) {
-	if dir.parent == nil {
+	if dir.parent == nil || dir.name == "root" {
 		return
 	}
 
@@ -134,9 +135,71 @@ func day7(path string) int {
 		&root,
 		func(dir *Dir) {
 			if dir.size < MAX_DIR_SIZE {
-				fmt.Println("-> ", dir.name)
 				result += int(dir.size)
 			}
+		},
+	)
+
+	return result
+}
+
+func day7_part2(path string) int {
+	file, err := os.Open(path)
+
+	defer file.Close()
+
+	if err != nil {
+		return 0
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var root Dir
+	root.name = "root"
+	root.parent = nil
+
+	current_dir := &root
+
+	for scanner.Scan() {
+		name, value := getLineData(scanner.Text())
+
+		if isFile(name) {
+			file_size, _ := strconv.Atoi(name)
+
+			current_dir.size += file_size
+		}
+
+		if isCmdChangeDir(name) {
+			if strings.Compare(value, COMMAND_BACK) == 0 {
+				current_dir = current_dir.parent
+			} else {
+				dir := newDir(value, current_dir)
+
+				current_dir.childrens = append(current_dir.childrens, dir)
+				current_dir = dir
+			}
+		}
+	}
+
+	var result int = 0
+
+	explorer(
+		&root,
+		func(dir *Dir) {
+		  updateParentSize(dir)
+		},
+	)
+
+  free_space := FILE_SYSTEM_SIZE - root.size
+  required := FREE_SPACE_REQUIRED - free_space
+
+	explorer(
+		&root,
+		func(dir *Dir) {
+      if dir.size > required && (dir.size < result || result == 0) {
+        result = dir.size
+      }
 		},
 	)
 
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -32,3 +32,32 @@ func TestDay7PartOne(t *testing.T) {
 		})
 	}
 }
+
+func TestDay7PartTwo(t *testing.T) {
+	tests := []struct {
+		name     string
+		path     string
+		expected int
+	}{
+		{
+			name:     "returns the correct value with test fixture",
+			path:     "./fixtures/test.txt",
+			expected: 24933642,
+		},
+		{
+			name:     "returns the correct value with puzzle fixture",
+			path:     "./fixtures/puzzle.txt",
+			expected: 2195372,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := day7_part2(test.path)
+
+			if test.expected != actual {
+				t.Errorf("Expected: %d; Actual: %d\n", test.expected, actual)
+			}
+		})
+	}
+}
